bbf34821020b444a6756974dc63cb4e3d613a2fc
Maicon Machado Gerardi da Silva <maicon.gerardi@dbccompany.com.br>
2021-09-20 19:02:16 -0300

    projeto inicial

---
+++ src/main/java/org/jornadatech/jornadaflix/JornadaflixApplication.java
@@ -0,0 +1,13 @@
+package org.jornadatech.jornadaflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornadaflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornadaflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jornadatech/jornadaflix/JornadaflixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jornadatech.jornadaflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
