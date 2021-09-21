dd784178e8a26023079b77d1e8fb6bb635f9df76
gsilvaro11 <guiuser1999@gmail.com>
2021-09-21 17:02:12 -0300

    Acionando properties

--- src/main/java/org/jornadatech/jornadaflix/JornadaflixApplication.java
+++ src/main/java/org/jornadatech/jornadaflix/JornadaflixApplication.java
@@ -1,10 +1,10 @@
 package org.jornadatech.jornadaflix;
-
+import java.sql.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JornadaflixApplication {
+public class JornadaflixApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JornadaflixApplication.class, args);
